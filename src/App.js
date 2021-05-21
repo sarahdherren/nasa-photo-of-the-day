172d81201b58639bd9c61a6486808334b60fe9db
Sarah D Herren <herren.sarah@icloud.com>
2021-05-21 09:53:58 -0500

    sketched wire frames/compoenent hierarchy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {
+    0 ? <h1>{1}</h1> : <h1>{0}</h1>
+  }
     </div>
   );
 }
